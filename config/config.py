dda7ad7ac39ef8e4dab5fdc1a4fc8a86a1212e52
NOBITA BOT MAKER <nishathakur.nkp@gmail.com>
2025-01-03 21:47:54 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -53,7 +53,7 @@ EXTRA_PLUGINS = getenv(
 
 EXTRA_PLUGINS_REPO = getenv(
     "EXTRA_PLUGINS_REPO",
-    "",
+    "https://github.com/Highpower143/ExtraPlugin",
 )
 # Fill here the external plugins repo where plugins that you want to load
 
@@ -144,7 +144,7 @@ TELEGRAM_DOWNLOAD_EDIT_SLEEP = int(getenv("TELEGRAM_EDIT_SLEEP", "5"))
 
 
 # Your Github Repo.. Will be shown on /start Command
-GITHUB_REPO = getenv("GITHUB_REPO", "https://github.com/vishalpandeynkp1/VIPNOBITAMUSIC_REPO")
+GITHUB_REPO = getenv("GITHUB_REPO", "https://github.com/vishalpandeynkp/VIPNOBITAMUSIC_REPO")
 
 
 # Spotify Client.. Get it from https://developer.spotify.com/dashboard
