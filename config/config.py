af4f6716462acb88714a3c0bf2ee5cf50e222c9f
NOBITA BOT MAKER <nishathakur.nkp@gmail.com>
2025-01-03 22:30:26 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -90,7 +90,7 @@ HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 # For customized or modified Repository
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/vishalpandeynkp/",
+    "https://github.com/Highpower143/NobitaBro",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 
